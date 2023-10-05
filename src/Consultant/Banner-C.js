0662c83b5da6c84532b87840142ef73e97ea5978
manon <derydtmanon@gmail.com>
2023-10-05 11:50:18 +0200

    Modifs Design et structuration du code

--- src/Consultant/Banner-C.js
+++ src/Consultant/Banner-C.js
@@ -3,7 +3,7 @@ import Fade from "react-reveal/Fade";
 class BannerC extends React.Component {
     render() {
         return (
-            <div className="content-headband">
+            <div className="content-headband-c">
 
                 <Fade top>
                     <h2 className="text-container"> Il est temps de <span className="red-color">gagner de l'argent.</span></h2>
